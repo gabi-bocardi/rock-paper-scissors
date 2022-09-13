d9fc1f3037668bcf4601daf14a26e811306650b6
gabi-bocardi <bocardigm@gmail.com>
2022-09-12 22:59:20 -0700

    Created a game funtion that loops the user through 5 rounds and announces a user. Had to make change to playRound to return different parameters

--- myscript.js
+++ myscript.js
@@ -5,24 +5,51 @@ function getComputerChoice(){
     return MOVES[random];
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = prompt("What's your move? Choose Rock, Paper or Scissor.").toUpperCase();
 
 function playRound(playerSelection,computerSelection){
-   let winMessage = `You win! ${playerSelection} beats ${computerSelection}`;
-   let loseMessage = `You lose! ${computerSelection} beats ${playerSelection}`;
-   let tieMessage = 'It\'s a tie!';
-   
+
     if ((playerSelection == MOVES[0] && computerSelection == MOVES[1]) || (playerSelection == MOVES[1] && computerSelection == MOVES[0]))
-        return playerSelection == MOVES[0]? loseMessage : winMessage;
-    if ((playerSelection == MOVES[0] && computerSelection == MOVES[2]) || (playerSelection == MOVES[2] && computerSelection == MOVES[0]))
-        return playerSelection == MOVES[2]? loseMessage : winMessage;
-    if ((playerSelection == MOVES[1] && computerSelection == MOVES[2]) || (playerSelection == MOVES[2] && computerSelection == MOVES[1]))
-        return playerSelection == MOVES[1]? loseMessage : winMessage;
-    if (playerSelection == computerSelection)
-        return tieMessage;
+        return playerSelection == MOVES[0]? "Computer" : "User";
+    else if ((playerSelection == MOVES[0] && computerSelection == MOVES[2]) || (playerSelection == MOVES[2] && computerSelection == MOVES[0]))
+        return playerSelection == MOVES[2]? "Computer" : "User";
+    else if ((playerSelection == MOVES[1] && computerSelection == MOVES[2]) || (playerSelection == MOVES[2] && computerSelection == MOVES[1]))
+        return playerSelection == MOVES[1]? "Computer" : "User";
+    else if (playerSelection == computerSelection)
+        return "Tie";
 
     }
 
+function Game(){
+    let computerPoints = 0;
+    let userPoints = 0;
     
+    for(let i = 0; i < 5; i++){
+        const computerSelection = getComputerChoice();
+        const playerSelection = prompt("What's your move? Choose Rock, Paper or Scissor.").toUpperCase();
+        let winner = playRound(playerSelection,computerSelection);
+        if (winner == "Computer"){
+            computerPoints++;
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+        }
+        else if (winner == "User"){
+            userPoints++;
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+        }
+        else
+            console.log('It\'s a tie!');
+        
+        console.log(`Rounds left: ${5 - (i + 1)}`)
+    }
+    if(computerPoints > userPoints)
+        console.log("Computer wins the game");
+    else if (computerPoints < userPoints)
+        console.log("You win the game");
+    else
+        console.log("It's a tie");
+
+    
+
+}
+
+Game();
 
